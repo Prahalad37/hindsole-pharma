ee92a95f0583fbfabca1aaab41bc7e54d4f56009
Prahalad Pal <prahalad@Prahalads-MacBook-Air.local>
2026-01-16 10:46:05 +0530

    Secured Firebase Keys

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,15 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// Aapki Config (Same to Same)
+// ✅ Ab ye Vercel aur Local .env file se keys uthayega (Secure)
 const firebaseConfig = {
-  apiKey: "AIzaSyAs-rI3t5qgRU8bbeJnYP5DO_tQ40KgNdg",
-  authDomain: "hindsole-pharma.firebaseapp.com",
-  projectId: "hindsole-pharma",
-  storageBucket: "hindsole-pharma.firebasestorage.app",
-  messagingSenderId: "804977906814",
-  appId: "1:804977906814:web:46cf0d78159c67eeafb42f",
-  measurementId: "G-LLLK89CS8H"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID
 };
 
 // Initialize Firebase
@@ -19,6 +18,4 @@ const app = initializeApp(firebaseConfig);
 // Export Services
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-
-// ✅ FIX: Naam badal kar 'provider' kar diya taaki Header.tsx ko mil jaye
 export const provider = new GoogleAuthProvider();
